9aba227430c27dc8f71fa9d2628c81c5037f44bd
marcyeah <128000400+marcyeah@users.noreply.github.com>
2023-07-03 06:15:51 -0500

    Update jugador.h

--- DoM/jugador/jugador.h
+++ DoM/jugador/jugador.h
@@ -2,6 +2,7 @@
 #define JUGADOR_H
 
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -14,15 +15,16 @@ private:
 
 public:
     jugador(const string& a, int p, int s, short v);
+    vector<string> nick;
+    vector<int> score;
     void setApodo(const string& a);
     void setPuntuacion(int p);
     void setSoles(int s);
     void setVida(short v);
-    string getApodo() const;
-    int getPuntuacion() const;
+    string getApodo(int n) const;
+    int getPuntuacion(string a) const;
     int getSoles() const;
     short getVida() const;
-    void start();
 };
 
 #endif
